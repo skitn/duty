15fed445ef077108727d9af6c9168a55fd120d15
skitn <tech.ntaka@gmail.com>
2017-11-25 01:44:00 +0900

    add default duty count

--- config/config.go
+++ config/config.go
@@ -1,7 +1,35 @@
 package config
 
+import (
+	"io/ioutil"
+
+	"github.com/BurntSushi/toml"
+)
+
+const (
+	DefaultDutyCount = 1
+)
+
 type Config struct {
-	DutyCount           int
-	Members             []string
-	CustomHolidays      []string
+	DutyCount      int
+	Members        []string
+	CustomHolidays []string
+}
+
+func Load(configPath string) (Config, error) {
+	buf, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	if err := toml.Unmarshal(buf, &config); err != nil {
+		return Config{}, err
+	}
+
+	if config.DutyCount <= 0 {
+		config.DutyCount = DefaultDutyCount
+	}
+
+	return config, nil
 }
--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/BurntSushi/toml"
 	"github.com/yut-kt/goholiday"
 
 	"github.com/skitn/duty/config"
@@ -20,7 +18,7 @@ func main() {
 		version    bool
 		configPath string
 		startDate  string
-		config     config.Config
+		conf       config.Config
 	)
 
 	flag.BoolVar(&version, "version", false, "Print version information and quit")
@@ -33,29 +31,22 @@ func main() {
 		os.Exit(0)
 	}
 
-	if configPath != "" {
-		buf, err := ioutil.ReadFile(configPath)
+	conf, err := config.Load(configPath)
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: %s", err))
+		os.Exit(1)
+	}
+
+	holidays := []time.Time{}
+	for i := 0; i < len(conf.CustomHolidays); i++ {
+		datetime, err := time.Parse("2006-01-02", conf.CustomHolidays[i])
 		if err != nil {
 			fmt.Println(fmt.Errorf("error: %s", err))
 			os.Exit(1)
 		}
-
-		if err := toml.Unmarshal(buf, &config); err != nil {
-			fmt.Println(fmt.Errorf("error: %s", err))
-			os.Exit(1)
-		}
-
-		holidays := []time.Time{}
-		for i := 0; i < len(config.CustomHolidays); i++ {
-			datetime, err := time.Parse("2006-01-02", config.CustomHolidays[i])
-			if err != nil {
-				fmt.Println(fmt.Errorf("error: %s", err))
-				os.Exit(1)
-			}
-			holidays = append(holidays, datetime)
-		}
-		goholiday.SetUniqueHolidays(holidays)
+		holidays = append(holidays, datetime)
 	}
+	goholiday.SetUniqueHolidays(holidays)
 
 	var currentDatePointer *time.Time
 	currentDate, err := time.Parse("2006-01-02", startDate)
@@ -66,17 +57,17 @@ func main() {
 	currentDatePointer = &currentDate
 
 	var dutyDays = []string{}
-	for i := 0; i < len(config.Members); i++ {
+	for i := 0; i < len(conf.Members); i++ {
 		dutyDays = nil
 		var j int
-		for j < config.DutyCount {
+		for j < conf.DutyCount {
 			if !goholiday.IsHoliday(currentDate) {
 				dutyDays = append(dutyDays, currentDate.Format("01/02"))
 				j++
 			}
 			*currentDatePointer = currentDatePointer.AddDate(0, 0, 1)
 		}
-		fmt.Printf("%s：%s\n", config.Members[i], strings.Join(dutyDays, ","))
+		fmt.Printf("%s：%s\n", conf.Members[i], strings.Join(dutyDays, ","))
 	}
 
 	os.Exit(0)
